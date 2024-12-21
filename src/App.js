a52e4e233b145a3ca4be08552c6e455c0e2789e4
Pallagani-Satish <satishpallagani9@gmail.com>
2024-12-21 20:24:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LandingPage from './pages/landing-page';
+
+function App() {
+  return (
+    <div className="container">
+   <LandingPage />
+    </div>
+  );
+}
+
+export default App;
