37e5ad216bdc8c8431e02542a2b1f281407b38f7
Pallagani-Satish <satishpallagani9@gmail.com>
2025-01-04 20:32:16 +0530

    BackedAdded

--- src/components/normal-header/normal-header-component.js
+++ src/components/normal-header/normal-header-component.js
@@ -1,23 +1,34 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../images/logo.svg";
 
-const NormalHeader = () => {
-  return (
-    <nav>
-      {/* Logo Section */}
-      <div className="logo">
-        <img src={logo} alt="Paytm Logo" />
-      </div>
+const NormalHeader = ({ isLoggedIn, setIsLoggedIn }) => {
+  const navigate = useNavigate();
 
-      {/* Navigation Links */}
-      <div className="menu">
-        <Link to="/">Home</Link>
-        <Link to="/mobile-recharge">Mobile Recharge</Link>
-        <Link to="/transfer">Transfer</Link>
-        <Link to="/check-balance">Check Balance</Link>
-        <Link to="/signin">Sign In</Link>
-      </div>
-    </nav>
+  const handleSignOut = () => {
+    setIsLoggedIn(false); // Log out the user
+    navigate("/"); // Redirect to landing page
+  };
+
+  return (
+    isLoggedIn && ( // Only show the navbar if the user is logged in
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="Paytm Logo" />
+        </div>
+
+        <div className="menu">
+          <Link to="/mobile-recharge">Mobile Recharge</Link>
+          <Link to="/transfer">Transfer</Link>
+          <Link to="/check-balance">Check Balance</Link>
+          <Link to="/transactions">Transaction History</Link>
+
+          <button onClick={handleSignOut} className="signout-btn">
+            Sign Out
+          </button>
+        </div>
+      </nav>
+    )
   );
 };
 
